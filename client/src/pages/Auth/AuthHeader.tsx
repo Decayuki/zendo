19eefde8152ac3bc0b13da9e21883c9fe6fa5dc1
JCGAN <aznchristophe@gmail.com>
2026-02-14 10:24:04 +0100

    feat: Logo and component Button/Input/Logo/Message and add exemple LoginPage

---
+++ client/src/pages/Auth/AuthHeader.tsx
@@ -0,0 +1,14 @@
+import "./Auth.css";
+import Logo from "../../components/Logo/Logo";
+
+const AuthHeader = () => {
+    return (
+        <div className="auth-header">
+            <Logo width={80} height={80} />
+            <h1 className="auth-title">ZENDO</h1>
+            <p className="auth-subtitle">L'artisanat en tout simplicité</p>
+        </div>
+    );
+};
+
+export default AuthHeader;
