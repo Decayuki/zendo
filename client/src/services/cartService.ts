708c805806617961e8e1e9a3dda839ee622197f3
Mariondevdebutante <marion.baraer292@gmail.com>
2026-02-24 00:12:20 +0100

    Page favoris + service addToCart

---
+++ client/src/services/cartService.ts
@@ -0,0 +1,32 @@
+// =============================================================
+// UTILITAIRE AJOUT AU PANIER
+// Contient la fonction addToCart qui envoie une requete POST à l'API pour ajouter un produit au panier de l'utilisateur
+// =============================================================
+
+const addToCart = (
+  // je récupere l'id du produit et les choix de l'utilisateur (couleur, taille, quantité) pour les envoyer à l'API
+  productId: string,
+  color: string,
+  size: string,
+  quantity: number,
+) => {
+  // j'envoie une requete POST à l'API pour ajouter le produit au panier de l'utilisateur
+  return fetch(`http://localhost:5001/api/cart/${productId}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
+    body: JSON.stringify({
+      color: color,
+      size: size,
+      quantity: quantity,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data.message;
+    });
+};
+
+export { addToCart };
